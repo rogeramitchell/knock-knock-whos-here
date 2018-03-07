dc3bc337e84db07c4b7472bcb094582e2083bc8c
Roger Mitchell <rogeramitchell@gmail.com>
2018-03-07 16:02:01 -0500

    working through the display of data, will handle cometd portion next

--- src/aura/RecordViewer/RecordViewerController.js
+++ src/aura/RecordViewer/RecordViewerController.js
@@ -1,4 +1,26 @@
 ({
-	myAction : function(component, event, helper) {
+	doInit : function(component, event, helper) {
+		let recordId = component.get("v.recordId");
+		let action = component.get("c.handleInit");
+
+		action.setParams({
+			recordId: recordId
+		});
+
+		action.setCallback(this, function(response) {
+			let responseState = response.getState();
+			if(responseState === "SUCCESS") {
+				let recordViewers = response.getReturnValue();
+				component.set("v.recordViewers", recordViewers);
+				console.log(recordViewers);
+			} else {
+				console.error("There were some errors! 🔥")
+				console.error(response);
+				console.error(responseState);
+				console.error(response.getReturnValue());
+			}
+		})
+
+		$A.enqueueAction(action);
 	}
 })
